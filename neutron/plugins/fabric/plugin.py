8d6f690994e3feb9ab93f9d4525bacf97eddc400
batmancn <batmanustc@gmail.com>
2017-03-01 09:38:55 +0800

    comment_mitaka, write FabricPlugin APIs based on NeutronPluginBaseV2, refer http://www.cnblogs.com/zhutianshi/p/3898491.html

--- neutron/plugins/fabric/plugin.py
+++ neutron/plugins/fabric/plugin.py
@@ -1,10 +1,35 @@
-from neutron.db import db_base_plugin_v2
+from neutron import neutron_plugin_base_v2
 from neutron.openstack.common import log
 
 LOG = log.getLogger(__name__)
 
-# use db_base_plugin_v2.NeutronDbPluginV2 to operate DB.
-class FabricPlugin(db_base_plugin_v2.NeutronDbPluginV2):
-
+class FabricPlugin(neutron_plugin_base_v2.NeutronPluginBaseV2):
     def __init__(self):
         LOG.info("FabricPlugin is started.")
+
+    def create_network(self, context, network):
+        # Create a network by using data from network dictionary
+        # Send back a dictionary to display created network's info
+        return network
+
+    def update_network(self, context, id, network):
+        # Update a created network matched by id with
+        # data in the network dictionary. Send back a
+        # dictionary to display the network's updated info
+        return network
+
+    def get_network(self, context, id, fields=None):
+        network = {}
+        # List information of a specific network matched by id
+        # and return it in a form of dictionary
+        return network
+
+    def get_networks(self, context, filters=None, fields=None):
+        network = {}
+        # List all networks that are active
+        return network
+
+    def delete_network(self, context, id):
+        # Delete a specific network matched by id
+        # return back the id of the network.
+        return id
