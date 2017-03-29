fb323c602171d1c3269e7ccdbf7ca31c0400af0f
Sam <batmanustc@gmail.com>
2017-03-29 09:55:04 +0800

    comment_ocata, add my_test API to L2 agent

--- neutron/tests/unit/plugins/ml2/drivers/openvswitch/agent/test_ovs_neutron_agent.py
+++ neutron/tests/unit/plugins/ml2/drivers/openvswitch/agent/test_ovs_neutron_agent.py
@@ -1067,7 +1067,7 @@ class TestOvsNeutronAgent(object):
             self.assertTrue(self.agent.fullsync)
 
     def test_my_test(self):
-        self.agent.my_test(port="port")
+        self.agent.my_test("unused_context", port="port")
         self.assertEqual("port:port ", self.agent.my_test_value)
 
     def test_port_update(self):
