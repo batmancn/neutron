d2addf41efeb0a3a4860cb05be73afe65a3638be
Sam <batmanustc@gmail.com>
2017-05-01 21:18:55 +0800

    comment_ocata, oslo_cliff comment

---
+++ learn/oslo_cliff.py
@@ -0,0 +1,12 @@
+"""
+1. 《openstack设计与实现》
+
+cliff是oslo的命令行框架，与OVS的命令行框架类似。
+参考《1》该框架主要：
+- cliff.app.App
+- cliff.commandmanager.CommandManager
+- cliff.command.Command
+- cliff.interactive.InteractiveApp
+"""
+
+
